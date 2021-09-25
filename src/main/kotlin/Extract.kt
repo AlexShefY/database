5ff390726b79d79f56affe9c4b44e49da8280816
first_project_Shefer <lora0207@list.ru>
2021-09-25 20:45:48 +0300

    В следующий файлах были изменены использования функций

--- src/main/kotlin/Extract.kt
+++ src/main/kotlin/Extract.kt
@@ -5,7 +5,7 @@ fun Extract(arr : List<String>){
     if(arr.size != 2){
         Error(Errors.Size)
     }
-    else if(!map.contains(arr[1])){
+    else if(!map.containsKey(arr[1])){
         Error(Errors.NotExists)
     }
     else{
--- src/main/kotlin/dataOut.kt
+++ src/main/kotlin/dataOut.kt
@@ -5,7 +5,7 @@ import java.io.File
  */
 fun dataOut(){
     File("file_data.txt").printWriter().use { out ->
-        map.forEach { key, value ->
+        map.forEach { (key, value) ->
             out.println("$key~$value")
         }
     }
