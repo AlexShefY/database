3b97c703bd46d5dad045514b7fd39b6f1f4ab659
first_project_Shefer <lora0207@list.ru>
2021-09-25 16:28:08 +0300

    Написан шаблон программы
    Определен формат взаимодействия с командной срокой. Также с файлом, предназначенным для хранения данных. Определены команды, которые будет выполнять программа.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,59 @@
+var map : MutableMap<String, String> = mutableMapOf()
 fun main(args: Array<String>) {
-    TODO()
+    if(args[0] == "start"){
+        print(">")
+        dataIn()
+        var s = readLine()!!
+        while(s != "finish"){
+            var arr = s.split(" ")
+            when(arr[0]){
+                "add" -> {
+
+                }
+                "extract" -> {
+
+                }
+                "in" -> {
+
+                }
+                "first" -> {
+
+                }
+                "next" -> {
+
+                }
+                "last" -> {
+
+                }
+                "previous" -> {
+
+                }
+                "out" -> {
+
+                }
+                "remove" -> {
+
+                }
+                "removeAll" -> {
+
+                }
+                "removeIf" -> {
+
+                }
+                "change" -> {
+
+                }
+                "filterOut" -> {
+
+                }
+            }
+            print(">")
+            s = readLine()!!
+        }
+        dataOut()
+    }
+    else{
+        println("Error")
+        println("You should look at the terms of use")
+    }
 }
