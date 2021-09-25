babec1c01d4c6a11e89e1b50aab659b8760756ae
first_project_Shefer <lora0207@list.ru>
2021-09-25 19:05:17 +0300

    Добавил взаимодействие с файлом, в котором хранятся данные

--- src/main/kotlin/dataOut.kt
+++ src/main/kotlin/dataOut.kt
@@ -1,4 +1,9 @@
+import java.io.File
 
 fun dataOut(){
-
+    File("file_data.txt").printWriter().use { out ->
+        map.forEach { key, value ->
+            out.println("$key~$value")
+        }
+    }
 }
--- src/main/kotlin/input.kt
+++ src/main/kotlin/input.kt
@@ -1,4 +1,10 @@
+import java.io.File
 
 fun dataIn(){
-
+    File("file_data.txt").useLines{
+        lines -> lines.forEach{
+            var pair = it.split("~")
+            map[pair[0]] = pair[1]
+        }
+    }
 }
