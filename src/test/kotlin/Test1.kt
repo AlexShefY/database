ed815f4fc86c6f69b0f51366997445ee83797988
first_project_Shefer <lora0207@list.ru>
2021-09-30 21:05:09 +0300

    Добавлен еще один тест

--- src/test/kotlin/Test1.kt
+++ src/test/kotlin/Test1.kt
@@ -38,6 +38,23 @@ internal class Test1 {
     }
     @Test
     fun test2(){
+        RemoveAll()
+        In(listOf("in", "word1"))
+        Remove(listOf("remove", "word2"))
+        Add(listOf("add", "key1", "value1"))
+        Add(listOf("add", "key11", "value2"))
+        Add(listOf("add", "key1", "value3"))
+        Extract(listOf("extract", "key1"))
+        Extract(listOf("extract", "key2"))
+        In(listOf("in"))
+        Add(listOf("add", "key3"))
+        RemoveIf(listOf("RemoveIf", "regex", "key1\\w*"))
+        Extract(listOf("extract", "key1"))
+        assertEquals(getOutput(), listOf("This key doesn`t exist", "This item doesn`t exist", "This item already exists", "value1", "This item doesn`t exist", "Invalid amount of input",
+            "Invalid amount of input", "This item doesn`t exist"))
+    }
+    @Test
+    fun testHashes(){
         assertEquals(hashString("abslsdjkfglhekdjbgfhtjrsk;eglrsEA29!", "SHA-256"), hashString("abslsdjkfglhekdjbgfhtjrsk;eglrsEA29!", "SHA-256"))
         assertEquals(hashString("mdckns0f421-3irlknsldjh2pwjnlnjj\\/.'::", "SHA-256"), hashString("mdckns0f421-3irlknsldjh2pwjnlnjj\\/.'::", "SHA-256"))
         assertNotEquals(hashString("abc", "SHA-256"), hashString("abcd", "SHA-256"))
